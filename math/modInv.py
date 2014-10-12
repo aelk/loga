6e41076603ede1db4dffe6cfeeac9a0cdf1a79b3
Andrew <aelk@users.noreply.github.com>
2014-10-12 10:34:18 -0500

    Updated function description

--- math/modInv.py
+++ math/modInv.py
@@ -1,7 +1,8 @@
 from exgcd import exgcd
 
 def modInv(a, m):
-    """Returns the modular multiplicative inverse of a modulo m"""
+    """Returns the modular multiplicative inverse of a modulo m:
+    i.e., x such that ax is congruent to 1 mod m."""
     gcd, x, y = exgcd(a, m)
     if gcd != 1:
         # a and m cannot be coprime
