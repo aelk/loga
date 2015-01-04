3252d1d713964359bbe3f9f57a6812d2cad77852
Andrew <aelk@users.noreply.github.com>
2015-01-03 18:19:27 -0600

    Update binarySearch.py

--- search/binarySearch.py
+++ search/binarySearch.py
@@ -1,10 +1,10 @@
 from bisect import bisect_left
 
-def search(arr, key, low = 0, high = None):
+def search(A, key, low = 0, high = None):
     if high is None:
-        high = len(a)
-    pos = bisect_left(arr, key, low, high)
-    if arr[pos] == key:
+        high = len(A)
+    pos = bisect_left(A, key, low, high)
+    if A[pos] == key:
     	return pos
     else:
     	return -1
