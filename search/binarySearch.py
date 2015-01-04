d067d94f28929ad5fa613a77b8b96e2fa305e24d
Andrew <aelk@users.noreply.github.com>
2015-01-04 08:39:01 -0600

    Replaced two if-else statements w/ ternary operators

--- search/binarySearch.py
+++ search/binarySearch.py
@@ -4,14 +4,10 @@ def search(A, key, low = 0, high = None):
     if len(A) == 0:
         return -1
     elif len(A) == 1:
-        if A[0] == key:
-            return 0
-        else: 
-            return -1
+        return 0 if (A[0] == key) else -1
+        
     if high is None:
         high = len(A)
     pos = bisect_left(A, key, low, high)
-    if A[pos] == key:
-    	return pos
-    else:
-    	return -1
+    
+    return pos if (A[pos] == key) else -1
