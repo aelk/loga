9ab95357062bf8fe37abb56e138f8fafcda6493a
Andrew <aelk@users.noreply.github.com>
2015-01-03 20:38:49 -0600

    Checked for edge cases

--- search/binarySearch.py
+++ search/binarySearch.py
@@ -1,6 +1,13 @@
 from bisect import bisect_left
 
 def search(A, key, low = 0, high = None):
+    if len(A) == 0:
+        return -1
+    elif len(A) == 1:
+        if A[0] == key:
+            return 0
+        else: 
+            return -1
     if high is None:
         high = len(A)
     pos = bisect_left(A, key, low, high)
