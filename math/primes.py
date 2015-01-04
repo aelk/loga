a634691f8da66628164a69d0098295ef13b524fa
Andrew <aelk@users.noreply.github.com>
2015-01-04 08:25:47 -0600

    Create primes.py

---
+++ math/primes.py
@@ -0,0 +1,11 @@
+def genPrimes(n):
+	'''Returns a list of primes below n.'''
+	numbers = set(range(n, 1, -1))
+	primes = []
+
+	while numbers:
+		p = numbers.pop()
+		primes.append(p)
+		# Remove each multiple of p from numbers:
+		numbers.difference_update(set(range(p * 2, n + 1, p)))
+	return primes
